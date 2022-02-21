ffd2d2b7ee05b4d275a0694266df509f4208cc30
L_Fazal <luqman_fz@yahoo.com>
2022-02-21 11:38:43 +0530

    Added home page

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
             <li><a href="./faq.php">FAQ</a></li>
             <li><a href="./aboutus.php">About Us</a></li>
             <li><a href="./index.php">Login</a></li>
+            <li><a href="./home.php">Home</a></li>
         </ul>
     </nav>
 </header>
